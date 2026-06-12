8f7beb94cf7d33746dfaf0deb02aee059496e02e
Anisa Leci <anisa@hirenove.com>
2026-06-12 18:18:45 +0200

    remove marquee ticker from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import SiteLayout from "@/components/SiteLayout";
 import Hero from "@/components/Hero";
-import Marquee from "@/components/Marquee";
 import WhyHireNove from "@/components/WhyHireNove";
 import IntegrationsSection from "@/components/IntegrationsSection";
 import HomeProcess from "@/components/HomeProcess";
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <SiteLayout>
       <Hero />
-      <Marquee />
       <HomeProcess />
       <WhyHireNove />
       <IntegrationsSection />
